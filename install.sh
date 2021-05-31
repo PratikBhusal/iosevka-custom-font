99d640034a54e1f8746c467e78eabef740979520
PratikBhusal <PratikBhusal@users.noreply.github.com>
2021-05-31 09:03:25 -0500

    Add material design icons and hyphen ligature

--- install.sh
+++ install.sh
@@ -72,20 +72,21 @@ for style in regular bold italic; do
         --fontawesomeextension \
         --fontlogos            \
         --octicons             \
+        --materialdesignicons  \
         ../iosevka-custom-$style.ttf
 done
 # Apply nerd-font-patcher }}}
 
 # Move fonts to user font directory {{{
 mv -i \
-    "Iosevka Nerd Font Plus Font Awesome Plus Font Awesome Extension Plus Octicons Plus Font Logos (Font Linux) Windows Compatible.ttf" \
+    "Iosevka Nerd Font Plus Font Awesome Plus Font Awesome Extension Plus Octicons Plus Font Logos (Font Linux) Plus Material Design Icons Windows Compatible.ttf" \
     "${XDG_DATA_HOME:-$HOME/.local/share}"/fonts/TTF/iosevka-custom-nerd-fonts-regular.ttf
 
 mv -i \
-    "Iosevka Bold Nerd Font Plus Font Awesome Plus Font Awesome Extension Plus Octicons Plus Font Logos (Font Linux) Windows Compatible.ttf" \
+    "Iosevka Bold Nerd Font Plus Font Awesome Plus Font Awesome Extension Plus Octicons Plus Font Logos (Font Linux) Plus Material Design Icons Windows Compatible.ttf" \
     "${XDG_DATA_HOME:-$HOME/.local/share}"/fonts/TTF/iosevka-custom-nerd-fonts-bold.ttf
 
 mv -i \
-    "Iosevka Italic Nerd Font Plus Font Awesome Plus Font Awesome Extension Plus Octicons Plus Font Logos (Font Linux) Windows Compatible.ttf" \
+    "Iosevka Italic Nerd Font Plus Font Awesome Plus Font Awesome Extension Plus Octicons Plus Font Logos (Font Linux) Plus Material Design Icons Windows Compatible.ttf" \
     "${XDG_DATA_HOME:-$HOME/.local/share}"/fonts/TTF/iosevka-custom-nerd-fonts-italic.ttf
 # Move fonts to user font directory }}}
